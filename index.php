159f6ddd53abdec807a144e3bb4602570247d731
Naufal Rahman Ariffiandy <98276019+NaufalRahman@users.noreply.github.com>
2026-04-20 19:50:04 +0700

    Add files via upload
    UTS WEB 2

---
+++ index.php
@@ -0,0 +1,52 @@
+<html>
+<head>
+    <title>Task Manager</title>
+</head>
+    <body>
+        <h1>Task Manager</h1>
+        <p>Welcome to the Task Manager!</p>
+
+    <!-- MENU START -->
+<div class="menu">
+
+    <a href="index.php">Home</a>
+
+    <a href="#">To Do</a>
+    <a href="#">In Progress</a>
+    <a href="#">Done</a>
+
+    <a href="manage-lists.php">Manage Lists</a>
+    </div>
+    <!-- MENU END -->
+
+    <!--Task List START -->
+
+    <div class="All Tasks">
+        <a Add Task</a>
+
+        <table>
+
+        <tr>
+            <th>Task ID</th>
+            <th>Task Name</th>
+            <th>Priority</th>
+            <th>Dateline</th>
+            <th>Actions</th>
+        </tr>
+
+        <tr>
+            <td>1</td>
+            <td>Task 1</td>
+            <td>High</td>
+            <td>2024-07-01</td>
+            <td>
+                <button>Edit</button>
+                <button>Delete</button>
+            </td>
+        </table>
+
+       
+    </body>
+
+
+</html>
---
+++ manage-list.php
@@ -0,0 +1,47 @@
+<html>
+<head>
+    <title>Task Manager</title>
+</head>
+<body>
+    <h1>Task Manager</h1>
+
+    <a href="index.php">Home</a>
+    
+<h3>Manage Task Lists</h3>
+<!-- Display the list of task lists -->
+ <div class="all-list"
+    <table>
+        <tr>
+            <th>List ID</th>
+            <th>List Name</th>
+            <th>Actions</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>To Do</td>
+            <td>
+                <button>Edit</button>
+                <button>Delete</button>
+            </td>
+        </tr>
+
+        <tr>
+            <td>2</td>
+            <td>In Progress</td>
+            <td>
+                <button>Edit</button>
+                <button>Delete</button>
+            </td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Done</td>
+            <td>
+                <button>Edit</button>
+                <button>Delete</button>
+            </td>
+        </tr>
+ </div>
+<!-- Display the list of task lists -->
+</body>
+</html>
